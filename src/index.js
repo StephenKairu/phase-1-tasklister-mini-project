9a0eaf0af2864aca25f16a57262090d8ea1ca460
Stephen <skairu09@gmail.com>
2022-08-28 18:52:03 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const input = document.getElementById("new-task-description");
+  const output = document.getElementById("tasks");
+
+  document.getElementById("create-task").addEventListener("click", (event) => {
+    event.preventDefault()
+    const list = document.createElement("li")
+    list.innerHTML = input.value
+    output.appendChild(list)
+  })
 });
